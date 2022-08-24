8ea2f968c332c8ed5e28d402be011f124197e9da
RAFAELREDONDO <ing.redondo.rafael@gmail.com>
2022-08-23 22:08:43 -0500

    Subiendo Proyecto

---
+++ SPRINT1/ManejoFinanzas/ManejoFinanzas/src/test/java/com/celulaMinTic/ManejoFinanzas/ManejoFinanzasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celulaMinTic.ManejoFinanzas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManejoFinanzasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
