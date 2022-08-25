72898014a3d853ef931af06dbab5882bb9a14430
edissonchamorroc <john.chamorro@udea.edu.co>
2022-08-25 10:29:27 -0500

    Se configura src

--- src/main/java/com/misiontic/manejofinanzas/ManejoFinanzasApplication.java
+++ src/main/java/com/misiontic/manejofinanzas/ManejoFinanzasApplication.java
@@ -2,12 +2,18 @@ package com.misiontic.manejofinanzas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+
 
 @SpringBootApplication
 public class ManejoFinanzasApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ManejoFinanzasApplication.class, args);
 	}
 
 }
+
+
